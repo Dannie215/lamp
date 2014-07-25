68e48b3e5582420c2be562a8917afa9b9d0b145a
Dannie <Dan@caviart.co>
2014-07-25 17:26:11 +0000

    loading lamp repo folder form vagrant

---
+++ article.php
@@ -0,0 +1,33 @@
+<?php
+$id = $_GET['id'];
+
+?>
+
+<?php
+
+$connection = mysql_connect('127.0.0.1', 'root', 'basketbal1');
+mysql_select_db('DANNIE_blog');
+// mysql_connect(‘SQLdata.com’, ‘Jones’, ‘un1c0rn’, ‘my_database’);
+
+
+// Check connection
+if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
+}
+
+if ($connection) {
+  //die('YAY: ' . mysql_errno());
+  // SQL Query
+  $result = mysql_query('SELECT id, title, author, body FROM articles where id = ' . $id );
+
+  // Fetch the data from the result
+  while ($article = mysql_fetch_array($result)) {
+    echo 
+         "<h1>" . "<a href='article.php?id=" . $article['id'] . "'>" .  $article['title'] . "</a>" . "</h1>" . "<br>" .
+         "<h1> "  . $article['author']  . "</h1>" .
+         " "  . $article['body']  . "<br>";
+  }
+}
+
+mysql_close($connection);
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+$connection = mysql_connect('127.0.0.1', 'root', 'basketbal1');
+mysql_select_db('DANNIE_blog');
+// mysql_connect(‘SQLdata.com’, ‘Jones’, ‘un1c0rn’, ‘my_database’);
+
+
+// Check connection
+if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
+}
+
+if ($connection) {
+  //die('YAY: ' . mysql_errno());
+  // SQL Query
+  $result = mysql_query('SELECT id, title, author, body FROM articles');
+
+  // Fetch the data from the result
+  while ($row = mysql_fetch_array($result)) {
+       
+       print '<!DOCTYPE html>
+          <html>
+          <head>
+          <title>Dannies First LAMP</title>
+          </head> ';
+
+
+       print '<body>';
+    echo //" " . $row['id'] . "<br>" .
+         
+         "<div>" . "<h2>" . "<a href='article.php?id=" . $row['id'] . "'>" .  $row['title'] . "</a>" . "</h2>" . "<br>" . "</div>" .
+
+         " "  . $row['author']  . "<br>" .
+         " "  . $row['body']  . "<br>";
+
+         print '</body>';
+         
+         print '</html>'; 
+
+  }
+}
+
+mysql_close($connection);
+?>
+
